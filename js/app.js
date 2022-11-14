66318c871f850dd70bd9d41a28f8c894a60a5985
NotMaxivelow01 <mfont@endenschool.fr>
2022-11-14 10:19:47 +0100

    push

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const carre = document.getElementById('carre');
+const carrebleu = document.getElementById('carrebleu')
+
+// carre.addEventListener('click');
+// function sayhello() {
+//     console.log("hello")
+// }
+
+carre.addEventListener('click', () => {
+    console.log("je suis rouge")
+})
+
+carrebleu.addEventListener('click', () => {
+    console.log('je suis bleu')
+})
